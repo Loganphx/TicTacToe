afcbdcdc5a3449c722f2aa6a38978b8dd3b554d6
Logan Ingram <loganphx1@gmail.com>
2025-03-09 19:44:49 -0700

    Added drawBoard method

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+void drawBoard(char board[3][3])
+{
+    std::cout << "-------------\n";
+    for(int i = 0; i < 3; i++) {
+        std::cout << "| ";
+        for (int j = 0; j < 3; j++) {
+            std::cout << board[i][j] << " | ";
+        }
+        std::cout << "\n-------------\n";
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    drawBoard(board);
     return 0;
 }
