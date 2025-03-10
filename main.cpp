fb0d54c118fc04c6a6e88de1557a69d748a823ea
Logan Ingram <loganphx1@gmail.com>
2025-03-09 19:58:39 -0700

    Added checkWin method

--- main.cpp
+++ main.cpp
@@ -12,8 +12,30 @@ void drawBoard(char board[3][3])
     }
 }
 
+bool checkWin(char board[3][3], char player) {
+    // Check rows, columns, and diagonals
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == player && board[i][1] == player
+            && board[i][2] == player)
+            return true;
+        if (board[0][i] == player && board[1][i] == player
+            && board[2][i] == player)
+            return true;
+    }
+    if (board[0][0] == player && board[1][1] == player
+        && board[2][2] == player)
+        return true;
+    if (board[0][2] == player && board[1][1] == player
+        && board[2][0] == player)
+        return true;
+    return false;
+}
 int main() {
     char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
     drawBoard(board);
+    char player = 'X';
+    bool gameOver = checkWin(board, player);
+    char didWin = !gameOver ? 'F' : 'T';
+    std::cout << didWin;
     return 0;
 }
